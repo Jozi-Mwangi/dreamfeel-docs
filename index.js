7c0f5a221c2f34c3e9ff5ce8f7e5e353d626a5f9
Jozi-Mwangi <mwasjozi@gmail.com>
2023-08-09 16:41:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { default: Link } = require("next/link");
+
+function LandingPage () {
+    return (
+        <div>
+            <h1>Landing Page</h1>
+            <p>Exolore our comprehensive documentation for dreamfeel</p>
+
+            <Link href="/pages" >
+                <a>Start Exporing</a>
+            </Link>
+        </div>
+    );
+}
+
+export default LandingPage;
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo:(
+    <>
+      <span style={{marginLeft: ".4em", fontWeight: 800}} >
+        Dreamfeel
+      </span>
+    </>
+  ),
+
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/Jozi-Mwangi/dreamfeel-docs.git",
   footer: {
     text: 'Nextra Docs Template',
   },
